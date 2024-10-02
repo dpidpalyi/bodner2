b308c58eb85438ce60a4b1b75840137fe9865ecf
dpidpalyi <dpidpalyi@mail.ru>
2024-10-02 16:56:28 +0500

    add ch9

---
+++ ch10/package_example/go.mod
@@ -0,0 +1,3 @@
+module github.com/learning-go-book-2e/package_example
+
+go 1.19
---
+++ ch10/package_example/math/math.go
@@ -0,0 +1,9 @@
+// Package math provides functions to work with numbers
+//
+// [Mathematics]: https://en.wikipedia.org/wiki/Mathematics
+package math
+
+// Double doubles the value of 'a' integer number
+func Double(a int) int {
+	return a * 2
+}
