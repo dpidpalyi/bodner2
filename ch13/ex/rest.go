f6506510c76f2eceb82b63d9b23014cd824136e4
dpidpalyi <dpidpalyi@mail.ru>
2024-10-15 11:44:06 +0500

    add ex2-3

--- ch13/ex/rest.go
+++ ch13/ex/rest.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
 )
+
+type JSONTime struct {
+	Day_of_week  string `json:"day_of_week"`
+	Day_of_month int    `json:"day_of_month"`
+	Month        string `json:"month"`
+	Year         int    `json:"year"`
+	Hour         int    `json:"hour"`
+	Minute       int    `json:"minute"`
+	Second       int    `json:"second"`
+}
 
 func MiddleWare(sl *slog.Logger, next func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +31,34 @@ func GetTime(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	t := time.Now().Format(time.RFC3339)
+	t := time.Now()
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(t))
+	var out []byte
+	if r.Header.Get("Accept") == "application/json" {
+		out = buildJSON(t)
+	} else {
+		out = buildText(t)
+	}
+	w.Write(out)
 	return
+}
+
+func buildJSON(now time.Time) []byte {
+	t := JSONTime{
+		Day_of_week:  now.Weekday().String(),
+		Day_of_month: now.Day(),
+		Month:        now.Month().String(),
+		Year:         now.Year(),
+		Hour:         now.Hour(),
+		Minute:       now.Minute(),
+		Second:       now.Second(),
+	}
+	out, _ := json.Marshal(t)
+	return out
+}
+
+func buildText(now time.Time) []byte {
+	return []byte(now.Format(time.RFC3339))
 }
 
 func main() {
